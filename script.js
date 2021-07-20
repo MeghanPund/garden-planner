b8b57a974c1b749a573e5ad1e398d3b094d2a0f3
Meghan Pund <meghan.pund@gmail.com>
2021-07-20 11:58:14 -0400

    broke getEmail function and fixed it

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function getEmail() {
-const userEmail = document.getElementById('email').value;
-console.log(userEmail);
+// Function to get the name input from the HTML form and display it at the top of the page
+function getName() {
+    const userName = document.getElementById('name').value;
+    document.getElementById("helloUser").innerHTML = `Hello, ${userName}! Welcome to Garden Planner.`;
+    }
 
-// if userEmail.includes("@") == true {
-    
-// } else {
-//     alert("Something appears to be wrong with the email address you have entered. Please double-check your entry.")
-// }
+// Function to get the email input from the HTML form, make sure it has an "@" and a ".", and alert user that email has been accepted
+function getEmail() {
+    const userEmail = document.getElementById('email').value;
+        
+    if (userEmail.includes("@") && userEmail.includes(".")) {
+        alert(`Thanks, ${userEmail}!`);
+    } else {
+        alert("Something appears to be wrong with the name you have entered. Please double-check your entry.");
+    }
 }
