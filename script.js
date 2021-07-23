130d7007f328a2bf947fd180940103c50c197cc7
Meghan Pund <meghan.pund@gmail.com>
2021-07-23 18:05:46 -0400

    trying so hard to make the drag and drop work the way I want it to

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.addEventListener("drop", function(event) {
 // Snapping begins here. I created a bunch of div elements within the garden div, and each div can hold one vegetable.
 
 document.addEventListener('DOMContentLoaded', function() {
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 5; i++) {
   var div = document.createElement('div');
   div.id = 'container';
   div.className = 'gardenDropBox';
